182c8bc3b76daa0fda670ded229a1f55e9962400
Svetlana Kutyova <kutyovas@yandex.ru>
2022-04-12 00:04:19 +0300

    class

--- src/ShopItem.js
+++ src/ShopItem.js
@@ -3,24 +3,33 @@ import PropTypes from 'prop-types';
 import ShopClass from './ShopClass';
 
 // function ShopItemFunc({item}) {//2 вариант
-function ShopItemFunc(props) {
-const { item } = props;
-   return (
-     <div class="main-content">
-       <h2>{item.brand}</h2>
-       <h1>{item.title}</h1>
-       <h3>{item.description}</h3>
-         <div class="description">
-           {item.descriptionFull}
-         </div>
-         <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-         <div class="divider"></div>
-         <div class="purchase-info">
-            <div class="price">{item.currency}{item.price}</div>
-            <button>Добавить в корзину</button>
-         </div>
+class ShopItemFunc extends React.Component {
+  /*constructor(props) {
+    super(props);
+    this.item = props.item; 
+  }
+  */
+
+
+render() {
+  const { item } = this.props;  
+  return (
+    <div class="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+        <div class="description">
+          {item.descriptionFull}
+        </div>
+      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+      <div class="divider"></div>
+      <div class="purchase-info">
+      <div class="price">{item.currency}{item.price}</div>
+        <button>Добавить в корзину</button>
       </div>
-   );
+    </div>
+  );
+}
 }
 
 ShopItemFunc.propTypes = {
