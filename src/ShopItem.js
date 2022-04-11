65197ce2e137509e8cd1457e47e5cf2f52707d68
Svetlana Kutyova <kutyovas@yandex.ru>
2022-04-11 20:46:37 +0300

    ready

---
+++ src/ShopItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types'; 
+import ShopClass from './ShopClass';
+
+// function ShopItemFunc({item}) {//2 вариант
+function ShopItemFunc(props) {
+const { item } = props;
+   return (
+     <div class="main-content">
+       <h2>{item.brand}</h2>
+       <h1>{item.title}</h1>
+       <h3>{item.description}</h3>
+         <div class="description">
+           {item.descriptionFull}
+         </div>
+         <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+         <div class="divider"></div>
+         <div class="purchase-info">
+            <div class="price">{item.currency}{item.price}</div>
+            <button>Добавить в корзину</button>
+         </div>
+      </div>
+   );
+}
+
+ShopItemFunc.propTypes = {
+  item: PropTypes.instanceOf(ShopClass).isRequired
+}
+
+export default ShopItemFunc;
